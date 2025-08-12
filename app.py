f9da65f31f5b253942d0cacd4ca7197c4ab06448
Muammer <63202870+mozdemir90@users.noreply.github.com>
2025-08-12 13:45:57 +0300

    Deployment dosyaları kaldırıldı - Uygulama deployment öncesi haline geri alındı

--- app.py
+++ app.py
@@ -500,5 +500,4 @@ def ekle():
     return render_template("ekle.html", skorlar=skorlar, haber=haber, secili_model=secili_model, secili_dl_model=secili_dl_model, mesaj=mesaj)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5050))
-    app.run(debug=False, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=5050)
